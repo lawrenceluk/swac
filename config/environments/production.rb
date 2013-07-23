9d507e67996b96cf2f23b2ac312d21deb205bd38
lawrluk <lluk@ucsd.edu>
2013-07-23 15:00:32 -0700

    more fixes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Swac::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # PRECOMPILE ASSETS
+  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 end
